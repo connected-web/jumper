84312d20ae16f0c16530f0608572a1af578ad83c
John Beech <johnbeech.github@mkv25.net>
2023-12-08 01:01:02 +0000

    Ignore example and test txt files as well

--- src/strategies/aoc.strategy.js
+++ src/strategies/aoc.strategy.js
@@ -4,9 +4,13 @@ function aocStrategy (repo, startwd) {
     { command: `git clone git@github.com:${repo}.git repos/${repo}`, cwd: startwd },
     { command: 'pwd', cwd: repowd },
     { command: 'echo "\ninput.txt" >> .gitignore', cwd: repowd, startwd },
+    { command: 'echo "\nexample.txt" >> .gitignore', cwd: repowd, startwd },
+    { command: 'echo "\ntes*.txt" >> .gitignore', cwd: repowd, startwd },
     { command: 'git add .gitignore', cwd: repowd, startwd },
     { command: 'git commit -m "Ignore input.txt based on AoC license restrictions"', cwd: repowd, startwd },
     { command: 'git filter-repo --path-glob **/input.txt --invert-paths --force', cwd: repowd, startwd },
+    { command: 'git filter-repo --path-glob **/example.txt --invert-paths --force', cwd: repowd, startwd },
+    { command: 'git filter-repo --path-glob **/tes*.txt --invert-paths --force', cwd: repowd, startwd },
     { command: `git push --set-upstream git@github.com:${repo}.git main --force`, cwd: repowd, startwd }
   ]
 
